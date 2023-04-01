6de8ca5f38da7a298288df88f92fb1d10beb4eaa
shutootaki <ootaki200109@gmail.com>
2023-04-01 17:14:00 +0900

    feat: ikkyu

---
+++ src/components/features/chat/api/getIkkyuAI.ts
@@ -0,0 +1,41 @@
+import { Prompt } from "../../../../utils/types";
+import { systemPrompt } from "../functions/systemPrompts/systemPrompt";
+
+export const getIkkyuAI = async (
+  prompt: string,
+  userName: string | undefined,
+  channelName: string | null,
+  ikkyuChatHistory: Prompt[],
+  setIkkyuChatHistory: React.Dispatch<React.SetStateAction<Prompt[]>>
+) => {
+  const message = JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages: ikkyuChatHistory.length
+      ? [
+          systemPrompt(userName, channelName),
+          ...ikkyuChatHistory.slice(-2),
+          { role: "user", content: prompt },
+        ]
+      : [
+          systemPrompt(userName, channelName),
+          { role: "user", content: prompt },
+        ],
+  });
+
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+    },
+    body: message,
+  });
+  const data = await res.json();
+
+  setIkkyuChatHistory((prevChatHistory) => [
+    ...prevChatHistory,
+    { role: "assistant", content: data.choices[0].message.content },
+  ]);
+
+  return data.choices[0].message.content;
+};
