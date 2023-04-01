4ff0e947b8c4519e4c3c9b25a74ae58e5af4c5f6
shutootaki <ootaki200109@gmail.com>
2023-04-01 17:13:47 +0900

    feat: goku

---
+++ src/components/features/chat/api/getGokuAI.ts
@@ -0,0 +1,41 @@
+import { Prompt } from "../../../../utils/types";
+import { systemPrompt } from "../functions/systemPrompts/systemPrompt";
+
+export const getGokuAI = async (
+  prompt: string,
+  userName: string | undefined,
+  channelName: string | null,
+  gokuChatHistory: Prompt[],
+  setGokuChatHistory: React.Dispatch<React.SetStateAction<Prompt[]>>
+) => {
+  const message = JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages: gokuChatHistory.length
+      ? [
+          systemPrompt(userName, channelName),
+          ...gokuChatHistory.slice(-2),
+          { role: "user", content: prompt },
+        ]
+      : [
+          systemPrompt(userName, channelName),
+          { role: "user", content: prompt },
+        ],
+  });
+
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+    },
+    body: message,
+  });
+  const data = await res.json();
+
+  setGokuChatHistory((prevChatHistory) => [
+    ...prevChatHistory,
+    { role: "assistant", content: data.choices[0].message.content },
+  ]);
+
+  return data.choices[0].message.content;
+};
